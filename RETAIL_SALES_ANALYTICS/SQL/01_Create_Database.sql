1b27182a1db43fe7582373d6b822d47f0cea07f1
Yamunasomashekar <yamunasomashekar123@gmail.com>
2026-08-11 23:19:39 +0530

    Delete RETAIL_SALES_ANALYTICS directory

--- RETAIL_SALES_ANALYTICS/SQL/01_Create_Database.sql
+++
@@ -1,7 +0,0 @@
--- Create a dedicated database for the Retail Sales Analytics project
-
-CREATE DATABASE RetailSalesAnalytics;
-GO
-
--- Confirm it was created
-SELECT name FROM sys.databases WHERE name = 'RetailSalesAnalytics';
